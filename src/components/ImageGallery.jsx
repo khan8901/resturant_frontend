97bc4ccd357f1ec6f80b1e0808922b3ef5784442
Ahmad Ghani <ahmadshahidghani@gmail.com>
2023-05-16 01:40:18 +0500

    font size of menu increased

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import styles from "../styles/ImageGallery.module.css"; // Import the CSS module
 
 const ImageGallery = () => {
-
-
-
-
   const images = [
     require("../assets/menu/one.jpg"),
     require("../assets/menu/two.jpg"),
@@ -13,19 +9,29 @@ const ImageGallery = () => {
     require("../assets/menu/four.jpg"),
     require("../assets/menu/five.jpg"),
     require("../assets/menu/six.jpg"),
+    require("../assets/menu/seven.jpg"),
+    require("../assets/menu/eight.jpg"),
+    require("../assets/menu/nine.jpg"),
+    require("../assets/menu/ten.jpg"),
   ];
 
-
-
-
-
-
   return (
-    <div className={styles.imageGallery}> {/* Apply CSS module class */}
-      <div className={styles.imageGalleryContainer}> {/* Apply CSS module class */}
+    <div className={styles.imageGallery}>
+      {" "}
+      {/* Apply CSS module class */}
+      <div className={styles.imageGalleryContainer}>
+        {" "}
+        {/* Apply CSS module class */}
         {images.map((image, index) => (
-          <div key={index} className={styles.imageContainer}> {/* Apply CSS module class */}
-            <img src={image} alt={`Image ${index + 1}`} className={styles.image} /> {/* Apply CSS module class */}
+          <div key={index} className={styles.imageContainer}>
+            {" "}
+            {/* Apply CSS module class */}
+            <img
+              src={image}
+              alt={`Image ${index + 1}`}
+              className={styles.image}
+            />{" "}
+            {/* Apply CSS module class */}
           </div>
         ))}
       </div>
